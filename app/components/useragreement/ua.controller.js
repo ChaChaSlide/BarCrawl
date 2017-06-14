929201c12148e774572f0e7f0a689170daa29fac
Cha'Sed <millercd@sa.edu>
2017-06-14 15:29:57 -0400

    added the index HTML input

---
+++ app/components/useragreement/ua.controller.js
@@ -0,0 +1,24 @@
+// class uaController {
+
+// 	constructor($rootScope, $interval) {
+// 		let ctrl = this;
+// 		ctrl.$rootScope = $rootScope;
+// 		console.log('ua constructor');
+
+// 	};
+
+// 	click(){
+// 		let ctrl = this;
+// 		 let x = document.getElementById("checkBox").checked;
+// 		 console.log(x);
+// 			(x ? console.log('box is checked') : alert('Please Verify Age'));
+// 		// console.log('button is being pressed');
+// 		console.log('hello world');
+// 	}
+// };
+
+
+
+// console.log('uaCtrl is working');
+
+// export default uaController;
